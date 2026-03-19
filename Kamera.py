ee31f693b9b6f3b1c0fee45d6e0c6bf089c7eb0a
s-h-i-j-m-e-o-l-n-p <simonade@stud.ntnu.no>
2026-03-19 14:34:34 +0100

    Fiksa en liten feil, se forrige commit

--- Kamera.py
+++ Kamera.py
@@ -3,7 +3,7 @@ from picamera2 import Picamera2
 
 def camera_take_photo_save():
     try:        
-        filename = filename = "/home/CrowBroBox/Desktop/trash.jpg"
+        filename = "/home/CrowBroBox/Desktop/trash.jpg"
         camera = Picamera2()
         camera.capture_file(filename)
         print(f"Photo saved as {filename}!")
